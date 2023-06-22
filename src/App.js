7db324d53bd362df8a71243f6045e076b32faf17
Phan Anh Nguyễn <phananhnguyen@Phans-MacBook-Pro.local>
2023-06-22 13:33:51 -0400

    finishes isLoading

--- src/App.js
+++ src/App.js
@@ -5,19 +5,23 @@ export default function App() {
   const [fromCur, setFromCur] = useState("EUR");
   const [toCur, setToCur] = useState("USD");
   const [converted, setConverted] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(
     function () {
       async function convert() {
+        setIsLoading(true);
         const res = await fetch(
-          `https://api.frankfurter.app/latest?${amount}=100&from=${fromCur}&to=${toCur}`
+          `https://api.frankfurter.app/latest?amount=${amount}&from=${fromCur}&to=${toCur}`
         );
         const data = await res.json();
         setConverted(data.rates[toCur]);
+        setIsLoading(false);
       }
+      if (fromCur === toCur) return setConverted(amount);
       convert();
     },
-    [amount, toCur, fromCur]
+    [amount, fromCur, toCur]
   );
   return (
     <div>
@@ -25,14 +29,23 @@ export default function App() {
         type="text"
         value={amount}
         onChange={(e) => setAmount(Number(e.target.value))}
+        disabled={isLoading}
       />
-      <select value={fromCur} onChange={(e) => setFromCur(e.target.value)}>
+      <select
+        value={fromCur}
+        onChange={(e) => setFromCur(e.target.value)}
+        disabled={isLoading}
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <select value={toCur} onChange={(e) => setToCur(e.target.value)}>
+      <select
+        value={toCur}
+        onChange={(e) => setToCur(e.target.value)}
+        disabled={isLoading}
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
